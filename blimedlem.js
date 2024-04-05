6263b186de8137c1384d97b65ef0d91210866073
anjahrot <anjaheggen@gmail.com>
2024-04-06 00:23:25 +0200

    Endra etter resultat på test2

--- blimedlem.js
+++ blimedlem.js
@@ -1,4 +1,5 @@
   const skjema = document.getElementById('skjema');
+  const melding = document.getElementById('melding');
   const takkMelding = document.getElementById('takk');
   
   skjema.addEventListener('submit', (event) => {
@@ -6,7 +7,11 @@
     // Sjekk om skjemaet er gyldig
     if (skjema.checkValidity()) {
       // Skriv inn koden som sender skjemaet til serveren her
-      
+      // Skjul skjema
+      skjema.style.display = 'none';
+     // Skjul melding
+     melding.style.display = 'none';
+     // Vis ny melding
       // Vis takkemeldingen
       //skjema.style.display = 'none';
       takkMelding.style.display = 'block';
